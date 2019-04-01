11ee4f75dce7c06efd774ef1681b7164ee31dda4
LAGonauta <lagonauta@gmail.com>
2019-04-01 09:15:30 -0300

    Initial test for PInvoke with .NET Standard

---
+++ TestPInvoke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AlureWrapper
+{
+    public class TestPInvoke
+    {
+        [DllImport("alure-c-interface", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int subtract(int a, int b);
+
+        [DllImport("alure-c-interface", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int add(int a, int b);
+    }
+}
